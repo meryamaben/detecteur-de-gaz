998de8b51b253b3d62f8950fb082151f18c39cc0
meryembenadou <meryembenadou@gmail.com>
2023-10-03 22:42:19 +0200

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'detecteur_de_gaz'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('detecteur_de_gaz');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('detecteur_de_gaz app is running!');
+  });
+});
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'objets-connectes-7338c',
+    appId: '1:657342261116:web:99c55c77814ee7bd4fa6f9',
+    databaseURL: 'https://objets-connectes-7338c-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'objets-connectes-7338c.appspot.com',
+    apiKey: 'AIzaSyD5poKGZ4sbTCv1aELB6SufT0alHhofyCk',
+    authDomain: 'objets-connectes-7338c.firebaseapp.com',
+    messagingSenderId: '657342261116',
+  },
+    production: false,
+
+};

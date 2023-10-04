7a4140febb2bca2d6b6fdd06970f19dfccbe4283
19fatimazahra <145855834+19fatimazahra@users.noreply.github.com>
2023-10-04 08:34:48 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
-
+import { FIREBASE_OPTIONS } from '@angular/fire/
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +17,7 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase())
   ],
-  providers: [],
+  providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
